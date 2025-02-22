91636960d21d84a83290a54b3533cd88990e6657
amir <a.nouri1997@gmail.com>
2025-02-22 19:00:57 +0330

    create basic files & Header section & -> NavBar -> create NavBasket

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+import Navigation from "./Navigation/Navigation"
+
+function Header() {
+    return(
+        <div className="container">
+<Navigation></Navigation>
+        </div>
+    )
+    
+}
+export default Header
---
+++ src/Header/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import './Navigation.css'
+import NavBasket from './NavBasket/NavBasket'
+
+function Navigation() {
+    return ( 
+        <section className='main_navigation'>
+        <NavBasket></NavBasket>
+        </section>
+        
+    )
+}
+export default Navigation
