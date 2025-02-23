2719e24e8fc15d350438694e7aab3e148a2c601b
Mohammad Azimi <mohammadazimi91@gmail.com>
2025-02-23 20:48:30 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import './App.css'
 import Header from './Header/Header'
 import Main from './Main/Main'
 
-
 function App() {
- 
-
   return (
     <>
       <Header></Header>
       <Main></Main>
-      
     </>
   )
 }
