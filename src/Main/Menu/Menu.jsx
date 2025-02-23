add2a84eba826d1ae3a2cc9c5fdf3dd0ab906672
amir <a.nouri1997@gmail.com>
2025-02-23 19:29:32 +0330

    create Menu Item selector files and folder & mount on menu

--- src/Main/Menu/Menu.jsx
+++ src/Main/Menu/Menu.jsx
@@ -1,11 +1,13 @@
 import './Menu.css'
 import MenuTitle from './MenuTitle/MenuTitle'
+import MenuItemSelector from './MenuItemSelector/MenuItemSelector'
 
 function Menu() {
     return (
         <section className="menu" id="menux">
           <div className="container">
             <MenuTitle></MenuTitle>
+            <MenuItemSelector></MenuItemSelector>
 
           </div>
         </section>
