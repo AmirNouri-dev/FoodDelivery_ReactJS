119a2d6bd97802456bb5e1f471fc00ab6202e848
amir <a.nouri1997@gmail.com>
2025-02-23 16:14:45 +0330

    create Nav Menu

--- src/Header/Navigation/Navigation.jsx
+++ src/Header/Navigation/Navigation.jsx
@@ -1,10 +1,12 @@
 import './Navigation.css'
 import NavBasket from './NavBasket/NavBasket'
+import NavMenu from './NavMenu/NavMenu'
 
 function Navigation() {
     return ( 
         <section className='main_navigation'>
         <NavBasket></NavBasket>
+        <NavMenu></NavMenu>
         </section>
         
     )
