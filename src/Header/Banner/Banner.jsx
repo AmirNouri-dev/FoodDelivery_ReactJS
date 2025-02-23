07024c3a7c9a4b0c5503f131896116f9a03af2ba
amir <a.nouri1997@gmail.com>
2025-02-23 18:57:55 +0330

    create Right Banner section & mount on Banner -> mount on header

--- src/Header/Banner/Banner.jsx
+++ src/Header/Banner/Banner.jsx
@@ -1,10 +1,12 @@
 import './Banner.css'
 import BannerLeftSection from "./BannerLeftSection/BannerLeftSection";
+import BannerRightSection from "./BannerRightSection/BannerRightSection";
 
 function Banner() {
     return(
         <section className="main_header">
 <BannerLeftSection></BannerLeftSection>
+<BannerRightSection></BannerRightSection>
         </section>
     )
 }
