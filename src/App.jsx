6a44d30511cba2ee9daaadd16c09eb76e9a81e17
amir <a.nouri1997@gmail.com>
2025-03-02 01:37:24 +0330

    make a modal component functional & class base - and add prop-types module for props type validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@
 import './App.css'
 import Header from './Header/Header'
 import Main from './Main/Main'
+import Modal from './Modal/Modal'
 
 function App() {
   return (
     <>
       <Header></Header>
       <Main></Main>
+      <Modal>
+         
+            <div className="modal_Btns">
+              <a href="#" className="modal_btn_yes modal_btn">Yes</a>
+              <a href="#" className="modal_btn_no modal_btn">No</a>
+            </div>
+      </Modal>
+      <Modal message="Do you want to exit?">
+      
+            <div className="modal_Btns">
+              <a href="#" className="modal_btn_yes modal_btn">Yes</a>
+              <a href="#" className="modal_btn_no modal_btn">No</a>
+            </div>
+      </Modal>
     </>
   )
 }
---
+++ src/Modal/Modal.jsx
@@ -0,0 +1,25 @@
+import './Modal.css'
+
+// export default function Modal({children}) {
+//   return (
+//     <div className='modal_container'>
+//      {children}
+//     </div>
+//   )
+// }
+
+import React from 'react'
+
+export default class Modal extends React.Component {
+    render(){
+        return(
+            <div className='modal_container'>
+                <p>{this.props.message}</p>
+                {this.props.children}
+            </div>
+       )
+    }
+}
+Modal.defaultProps = {
+    message : 'شما موفق شدید'
+}
