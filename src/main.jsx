b8c7a04ba522416456ca1c6b6d7f2a4fd45ebf5c
amir <a.nouri1997@gmail.com>
2025-04-24 02:46:47 +0330

    یثحمخغ

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/FoodDelivery_ReactJS">
     <App />
   </BrowserRouter>
 );
